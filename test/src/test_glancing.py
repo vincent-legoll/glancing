9f4cde4412306dcef52bc7d9fc412faf20c25259
Vincent Legoll <vincent.legoll@idgrilles.fr>
2015-07-16 15:55:27 +0200

    Initial testing framework for glancing

---
+++ test/src/test_glancing.py
@@ -0,0 +1,13 @@
+#! /usr/bin/env python
+
+import sys
+
+from utils import get_local_path
+
+# Setup PYTHONPATH for glancing
+sys.path.append(get_local_path('..', '..', 'src'))
+
+import glancing
+
+def Glancing_test():
+    pass
