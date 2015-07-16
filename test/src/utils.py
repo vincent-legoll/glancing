ee77c4965fec4c26ab599f681d1ffa2ed2c02b0f
Vincent Legoll <vincent.legoll@idgrilles.fr>
2015-07-16 15:23:02 +0200

    Add an utility module
    with functions to help test & bench code
    to find their data or dependencies in a
    relative path to themselves

---
+++ test/src/utils.py
@@ -0,0 +1,13 @@
+#! /usr/bin/env python
+
+import os
+
+def mod_path():
+    file_myself = __file__ or sys.argv[0]
+    ret_path = os.path.dirname(file_myself)
+    if not os.path.isabs(ret_path):
+        ret_path = os.path.join(os.getcwd(), ret_path)
+    return os.path.realpath(ret_path)
+
+def get_local_path(*args):
+    return os.path.realpath(os.path.join(mod_path(), *args))
