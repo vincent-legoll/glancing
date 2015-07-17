a751dd83735055ed63076d9c3ee65b7c18a37177
Vincent Legoll <vincent.legoll@idgrilles.fr>
2015-07-17 15:44:34 +0200

    Add profiling parameter handling

--- run_tests.sh
+++ run_tests.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
 COVERAGE_OPTS="--with-coverage --cover-branches --cover-html --cover-inclusive --cover-tests --cover-package=glancing,multihash,metadata,decompressor,utils,test_glancing,test_multihash,test_metadata,test_decompressor"
+PROFILE_OPTS="" # --with-profile
 
-nosetests --exe ${COVERAGE_OPTS}
+nosetests --exe ${COVERAGE_OPTS} ${PROFILE_OPTS}
