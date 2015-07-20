a723b3e1860cc5ae41e07db6938adc72c2d6049a
Vincent Legoll <vincent.legoll@idgrilles.fr>
2015-07-20 16:30:22 +0200

    Add a devnull context-manager

--- test/src/utils.py
+++ test/src/utils.py
@@ -1,6 +1,11 @@
 #! /usr/bin/env python
 
+from __future__ import print_function
+
 import os
+import sys
+
+from contextlib import contextmanager
 
 def mod_path():
     file_myself = __file__ or sys.argv[0]
@@ -11,3 +16,16 @@ def mod_path():
 
 def get_local_path(*args):
     return os.path.realpath(os.path.join(mod_path(), *args))
+
+class devnull(object):
+    
+    def __init__(self, iodesc_name):
+        self._oldiodesc_name = iodesc_name
+
+    def __enter__(self):
+        self._oldiodesc = sys.__dict__[self._oldiodesc_name]
+        sys.__dict__[self._oldiodesc_name] = open(os.devnull, 'w+b')
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        sys.__dict__[self._oldiodesc_name].close()
+        sys.__dict__[self._oldiodesc_name] = self._oldiodesc
