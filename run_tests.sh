b0762f2dc32d299617af9599666f84cdd3e64f19
Vincent Legoll <vincent.legoll@idgrilles.fr>
2015-07-20 14:00:28 +0200

    - Pass additional CLI args through - Use local cloud-in-VMs config, if running

--- run_tests.sh
+++ run_tests.sh
@@ -3,4 +3,10 @@
 COVERAGE_OPTS="--with-coverage --cover-branches --cover-html --cover-inclusive --cover-tests --cover-package=glancing,multihash,metadata,decompressor,utils,test_glancing,test_multihash,test_metadata,test_decompressor"
 PROFILE_OPTS="" # --with-profile
 
-nosetests --exe ${COVERAGE_OPTS} ${PROFILE_OPTS}
+# If our local cloud is running, use it
+ps faux | grep [c]7-ctrl > /dev/null 2>&1
+if [ $? -eq 0 ]; then
+  . ~/openstack/cloud_auth_local.sh
+fi
+
+nosetests --exe ${COVERAGE_OPTS} ${PROFILE_OPTS} $*
