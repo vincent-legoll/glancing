9e1b88c50fb215e20a8faa3d096fdf69d6321a1b
Vincent Legoll <vincent.legoll@idgrilles.fr>
2015-07-21 11:07:53 +0200

    Add a test for json load failures

--- test/src/test_metadata.py
+++ test/src/test_metadata.py
@@ -1,9 +1,10 @@
 #! /usr/bin/env python
 
+import os
 import sys
 import unittest
 
-from utils import get_local_path
+from utils import get_local_path, devnull
 
 # Setup PYTHONPATH for metadata
 sys.path.append(get_local_path('..', '..', 'src'))
@@ -20,3 +21,15 @@ class TestMetaData(unittest.TestCase):
         self.assertIn('url', md)
         self.assertEqual(md['url'],
                          'http://appliances.stratuslab.eu/images/base/CentOS-7-Server-x86_64/1.1/CentOS-7-Server-x86_64.dsk.gz')
+
+    def metadata_test_not_dict(self):
+        fn = 'test.json'
+        if os.path.exists(fn):
+            os.remove(fn)
+        with open(fn, 'wb') as jsonf:
+            jsonf.write('""\n')
+        with devnull('stderr'):
+            with self.assertRaises(ValueError):
+                m = metadata.MetaStratusLab(fn)
+        if os.path.exists(fn):
+            os.remove(fn)
