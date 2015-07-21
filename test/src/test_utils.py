52616fe3106f98870c485cf7b59eab48803b1ef3
Vincent Legoll <vincent.legoll@idgrilles.fr>
2015-07-21 15:53:50 +0200

    - Fix pep8 error - Make devnull test more robust - Add environ context-manager test

--- test/src/test_utils.py
+++ test/src/test_utils.py
@@ -14,8 +14,8 @@ class TestUtils(unittest.TestCase):
         self.assertTrue(utils.mod_path().endswith('/glancing/test/src'))
 
     def utils_test_get_local_path(self):
-        self.assertTrue(utils.get_local_path('toto', 'titi.txt').endswith(
-        '/glancing/test/src/toto/titi.txt'))
+        lp = utils.get_local_path('toto', 'titi.txt')
+        self.assertTrue(lp.endswith('/glancing/test/src/toto/titi.txt'))
 
     def utils_test_devnull(self):
         if os.path.exists('test_devnull_out.txt'):
@@ -24,6 +24,8 @@ class TestUtils(unittest.TestCase):
             os.remove('test_devnull_err.txt')
         with open('test_devnull_out.txt', 'wb') as out:
             with open('test_devnull_err.txt', 'wb') as err:
+                old_out = sys.stdout
+                old_err = sys.stderr
                 sys.stdout = out
                 sys.stderr = err
 
@@ -32,9 +34,35 @@ class TestUtils(unittest.TestCase):
                     print('sareum', file=sys.stdout)
                     print('sonreup', file=sys.stderr)
                 print('samarchedenouveau', file=sys.stdout)
+
+                sys.stdout = old_out
+                sys.stderr = old_err
+
         self.assertEqual(open('test_devnull_err.txt', 'rb').read(), 'sonreup\n')
         self.assertEqual(open('test_devnull_out.txt', 'rb').read(), 'samarche\nsamarchedenouveau\n')
         if os.path.exists('test_devnull_out.txt'):
             os.remove('test_devnull_out.txt')
         if os.path.exists('test_devnull_err.txt'):
             os.remove('test_devnull_err.txt')
+
+    def utils_test_environ_not_existent_val(self):
+        self.assertTrue('TOTO' not in os.environ)
+        with utils.environ('TOTO', 'titi'):
+            self.assertEqual('titi', os.environ['TOTO'])
+        self.assertTrue('TOTO' not in os.environ)
+
+    def utils_test_environ_not_existent_no_val(self):
+        self.assertTrue('TOTO' not in os.environ)
+        with utils.environ('TOTO'):
+            self.assertEqual('', os.environ['TOTO'])
+        self.assertTrue('TOTO' not in os.environ)
+
+    def utils_test_environ_existent_val(self):
+        self.assertTrue('TOTO' not in os.environ)
+        os.environ['TOTO'] = 'tutu'
+        self.assertEqual('tutu', os.environ['TOTO'])
+        with utils.environ('TOTO', 'titi'):
+            self.assertEqual('titi', os.environ['TOTO'])
+        self.assertEqual('tutu', os.environ['TOTO'])
+        del os.environ['TOTO']
+        self.assertTrue('TOTO' not in os.environ)
