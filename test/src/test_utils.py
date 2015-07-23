79fa5c30ec04aa00594971f7d7f497be0164990e
Vincent Legoll <vincent.legoll@idgrilles.fr>
2015-07-23 15:07:39 +0200

    Add more tests

--- test/src/test_utils.py
+++ test/src/test_utils.py
@@ -5,6 +5,7 @@ from __future__ import print_function
 import os
 import sys
 import unittest
+import StringIO
 
 from tutils import get_local_path
 
@@ -15,8 +16,42 @@ import utils
 
 class TestUtils(unittest.TestCase):
 
+    def test_get_verbose_is_boolean(self):
+        v = utils.get_verbose()
+        self.assertTrue((v is True) or (v is False))
+
+    def test_set_verbose_toggle(self):
+        v = utils.get_verbose()
+        utils.set_verbose()
+        self.assertFalse(v == utils.get_verbose())
+        utils.set_verbose()
+        self.assertTrue(v == utils.get_verbose())
+
+    def test_set_verbose_value(self):
+        v = utils.get_verbose()
+        utils.set_verbose(True)
+        self.assertTrue(utils.get_verbose() is True)
+        utils.set_verbose(False)
+        self.assertTrue(utils.get_verbose() is False)
+        utils.set_verbose(v)
+        self.assertTrue(utils.get_verbose() == v)
+
+    def test_vprint(self):
+        v = utils.get_verbose()
+        utils.set_verbose(True)
+        with utils.stringio() as output:
+            with utils.redirect('stdout', output):
+                utils.vprint('TOTOTITI', utils.test_name())
+                self.assertEqual(utils.test_name() + ': TOTOTITI\n', output.getvalue())
+            self.assertEqual(utils.test_name() + ': TOTOTITI\n', output.getvalue())
+        with self.assertRaises(ValueError):
+            output.getvalue()
+        utils.set_verbose(v)
+
     def utils_test_test_name(self):
         self.assertEqual(utils.test_name(), 'utils_test_test_name')
+
+class TestUtilsRun(unittest.TestCase):
 
     def utils_test_run_true(self):
         self.assertTrue(utils.run(['true'])[0])
@@ -25,9 +60,14 @@ class TestUtils(unittest.TestCase):
         self.assertFalse(utils.run(['n o t h i n g'])[0])
 
     def utils_test_run_false(self):
-        utils.set_verbose(True)
         self.assertFalse(utils.run(['false'])[0])
-        utils.set_verbose(False)
+
+    def utils_test_run_false(self):
+        good, retcode, out, err = utils.run(['ls', '--format'], out=True, err=True)
+        self.assertFalse(good)
+        self.assertFalse(0 == retcode)
+        self.assertEqual('', out)
+        self.assertFalse('' == err)
 
     def utils_test_run_not_in_path(self):
         with utils.environ('PATH'):
@@ -48,6 +88,81 @@ class TestUtils(unittest.TestCase):
         self.assertTrue(retcode == 0)
         self.assertEqual(out, 'TOTO')
         self.assertIsNone(err)
+
+class TestUtilsStringio(unittest.TestCase):
+
+    def test_stringio_write(self):
+        with utils.stringio() as out:
+            out.write('TOTO')
+            self.assertEqual('TOTO', out.getvalue())
+        self.assertTrue(out.closed)
+
+    def test_stringio_read(self):
+        with utils.stringio() as out:
+            self.assertEqual('', out.getvalue())
+            self.assertEqual('', out.read())
+            out.write('TOTO')
+            self.assertEqual('TOTO', out.getvalue())
+            self.assertEqual('', out.read())
+            out.seek(0)
+            self.assertEqual('TOTO', out.getvalue())
+            self.assertEqual('TOTO', out.read())
+            out.seek(0, os.SEEK_END)
+            out.write('TITI')
+            self.assertEqual('TOTOTITI', out.getvalue())
+            self.assertEqual('', out.read())
+            out.seek(0)
+            self.assertEqual('TOTOTITI', out.read())
+            out.seek(0)
+            out.write('TITI')
+            self.assertEqual('TITITITI', out.getvalue())
+            self.assertEqual('TITI', out.read())
+            out.seek(0)
+            self.assertEqual('TITITITI', out.read())
+        self.assertTrue(out.closed)
+
+    def test_fileio_read(self):
+        test_file = 'test.txt'
+        utils.run(['touch', test_file])
+        with utils.cleanup(['rm', test_file]):
+            with open(test_file, 'rw+b') as out:
+                self.assertEqual('', out.read())
+                out.write('TOTO')
+                self.assertEqual('', out.read())
+                out.seek(0)
+                self.assertEqual('TOTO', out.read())
+                out.seek(0, os.SEEK_END)
+                out.write('TITI')
+                self.assertEqual('', out.read())
+                out.seek(0)
+                self.assertEqual('TOTOTITI', out.read())
+                out.seek(0)
+                out.write('TITI')
+                self.assertEqual('TITI', out.read())
+                out.seek(0)
+                self.assertEqual('TITITITI', out.read())
+            self.assertTrue(out.closed)
+
+class TestUtilsRedirect(unittest.TestCase):
+
+    def test_print(self):
+        with utils.stringio() as output:
+            with utils.redirect('stdout', output):
+                print('TOTOTITI')
+                self.assertEqual('TOTOTITI\n', output.getvalue())
+            self.assertEqual('TOTOTITI\n', output.getvalue())
+        with self.assertRaises(ValueError):
+            output.getvalue()
+
+    def test_close(self):
+        tmp = None
+        with utils.redirect('stdout'):
+            print('toto')
+            print('toto', file=sys.stdout)
+            self.assertFalse(sys.stdout.closed)
+            tmp = sys.stdout
+        self.assertFalse(sys.stdout.closed)
+        self.assertTrue(tmp.closed)
 
 class TestUtilsDevnull(unittest.TestCase):
 
@@ -80,6 +195,19 @@ class TestUtilsDevnull(unittest.TestCase):
         self._cleanup_files()
 
     def utils_test_devnull_print(self):
+
+        print('samarche')
+        with utils.devnull('stdout'):
+            print('sareum')
+            print('sonreup', file=sys.stderr)
+        print('samarchedenouveau')
+
+        self._commit()
+
+        self.assertEqual(open('test_devnull_err.txt', 'rb').read(), 'sonreup\n')
+        self.assertEqual(open('test_devnull_out.txt', 'rb').read(), 'samarche\nsamarchedenouveau\n')
+
+    def utils_test_devnull_print_explicit(self):
 
         print('samarche', file=sys.stdout)
         with utils.devnull('stdout'):
