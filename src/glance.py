50a4fdddf677bdf32b1ca3e202c719dd2731a9fe
Vincent Legoll <vincent.legoll@idgrilles.fr>
2015-08-12 16:17:35 +0200

    More tests & debug

--- src/glance.py
+++ src/glance.py
@@ -1,6 +1,13 @@
 #! /usr/bin/env python
 
+from __future__ import print_function
+
+import sys
+import uuid
+import argparse
+
 import utils
+import openstack_out
 
 from utils import vprint
 
@@ -30,13 +37,61 @@ def glance_import(base, md5=None, name=None, diskformat=None):
     return ok
 
 def glance_exists(name):
-    cmd = _GLANCE_CMD + ['image-show', name]
+    if type(name) is not str:
+        raise TypeError
+    return len(glance_ids([name])) > 0
+
+def glance_image_list():
+    cmd = _GLANCE_CMD + ['image-list']
     ok, retcode, out, err = utils.run(cmd, out=True, err=True)
     if not ok:
-        vprint('failed to test existence from glance: ' + name)
+        vprint('failed to get image-list from glance: ' + name)
         vprint('stdout=' + out)
         vprint('stderr=' + err)
-    return ok
+        return None
+    return out
+
+def glance_ids(names):
+    ret = set()
+    # Some are already IDs
+    if type(names) is str:
+        names = [names]
+    if type(names) is list:
+        if names:
+            for name in names:
+                if type(name) is str:
+                    try:
+                        uuid.UUID(name)
+                        names.remove(name)
+                        ret.add(name)
+                    except ValueError as e:
+                        if e.args != ('badly formed hexadecimal UUID string',) :
+                            raise e
+                    except TypeError as e:
+                        if e.args != ('need one of hex, bytes, bytes_le, fields, or int',):
+                            raise e
+        # The rest (if anything left) is assumed to be "real" names
+        if names:
+            il = glance_image_list()
+            if il:
+                h, b = openstack_out.parse_block(il)
+                for image_id, image_name, _, _, _, _ in b:
+                    if image_name in names:
+                        ret.add(image_id)
+    return ret
+
+def glance_delete_all(names, quiet=False):
+    ret = False
+    all_images_ids = glance_ids(names)
+    if all_images_ids:
+        ret = glance_delete_ids(all_images_ids, quiet=quiet)
+    return ret
+
+def glance_delete_ids(ids, quiet=False):
+    ret = True
+    for image_id in ids:
+        ret &= glance_delete(image_id, quiet=quiet)
+    return ret
 
 def glance_delete(name, quiet=False):
     cmd = _GLANCE_CMD + ['image-delete', name]
@@ -59,3 +114,33 @@ def glance_download(name, fn_local):
         vprint('stdout=' + out)
         vprint('stderr=' + err)
     return ok
+
+# Handle CLI options
+def do_argparse(sys_argv):
+    parser = argparse.ArgumentParser(description='Manage glance VM images')
+
+    # Global options
+    parser.add_argument('-v', '--verbose', action='store_true',
+                        help='display additional information')
+
+    group = parser.add_mutually_exclusive_group()
+    group.add_argument('-d', '--delete', dest='delete', metavar='NAME', nargs='+',
+                       help='delete all images with the same name as '
+                            'the specified VM')
+
+    args = parser.parse_args(sys_argv)
+
+    if args.verbose:
+        utils.set_verbose(True)
+        vprint('verbose mode')
+
+    return args
+
+def main(sys_argv=sys.argv):
+    args = do_argparse(sys_argv)
+    if args.delete:
+        glance_delete_all(args.delete)
+    return True
+
+if __name__ == '__main__':
+    main()
--- test/src/test_glance.py
+++ test/src/test_glance.py
@@ -19,44 +19,114 @@ class TestGlance(unittest.TestCase):
     def test_ok(self):
         self.assertTrue(glance.glance_ok())
 
-class TestGlanceFixture(unittest.TestCase):
-
-    def setUp(self):
-        glance.glance_delete('test-glance-img', quiet=True)
-
-    def tearDown(self):
-        glance.glance_delete('test-glance-img', quiet=True)
-
-    def test_exists(self):
+    def test_exists_raise(self):
         with self.assertRaises(TypeError):
             glance.glance_exists(None)
         with self.assertRaises(TypeError):
             glance.glance_exists(True)
         with self.assertRaises(TypeError):
             glance.glance_exists(False)
-        self.assertFalse(glance.glance_exists(''))
-        self.assertFalse(glance.glance_exists('test-glance-img'))
-        glance.glance_import('/dev/null', name='test-glance-img')
-        self.assertTrue(glance.glance_exists('test-glance-img'))
+        with self.assertRaises(TypeError):
+            glance.glance_exists([])
 
-    def test_import(self):
-        self.assertFalse(glance.glance_exists('test-glance-img'))
-        self.assertTrue(glance.glance_import('/dev/null', name='test-glance-img'))
-        self.assertTrue(glance.glance_exists('test-glance-img'))
+    def test_exists_false(self):
+        self.assertFalse(glance.glance_exists(''))
+        self.assertFalse(glance.glance_exists('Nonexistent'))
+
+    def test_glance_ids_single(self):
+        self.assertEqual(set(), glance.glance_ids(None))
+        self.assertEqual(set(), glance.glance_ids(True))
+        self.assertEqual(set(), glance.glance_ids(False))
+        self.assertEqual(set(), glance.glance_ids(''))
+        self.assertEqual(set(), glance.glance_ids([]))
+
+    def test_glance_ids_list_single(self):
+        self.assertEqual(set(), glance.glance_ids([None]))
+        self.assertEqual(set(), glance.glance_ids([True]))
+        self.assertEqual(set(), glance.glance_ids([False]))
+        self.assertEqual(set(), glance.glance_ids(['']))
+        self.assertEqual(set(), glance.glance_ids([[]]))
+
+    def test_glance_ids_list(self):
+        self.assertEqual(set(), glance.glance_ids([None, True, False]))
+        self.assertEqual(set(), glance.glance_ids(['', '']))
+
+_IMG_NAME = 'test-glance-img'
+
+class TestGlanceFixture(unittest.TestCase):
+
+    def setUp(self):
+        glance.glance_delete_all(_IMG_NAME, quiet=True)
+
+    def tearDown(self):
+        glance.glance_delete_all(_IMG_NAME, quiet=True)
+
+class TestGlanceMain(TestGlanceFixture):
+
+    def test_main_ok(self):
+        self.assertFalse(glance.glance_exists(_IMG_NAME))
+        self.assertTrue(glance.glance_import('/dev/null', name=_IMG_NAME, diskformat='raw'))
+        self.assertTrue(glance.glance_exists(_IMG_NAME))
+        self.assertTrue(glance.main(['-d', _IMG_NAME]))
+        self.assertFalse(glance.glance_exists(_IMG_NAME))
+
+    def test_main_ok_verbose(self):
+        self.assertFalse(glance.glance_exists(_IMG_NAME))
+        self.assertTrue(glance.glance_import('/dev/null', name=_IMG_NAME, diskformat='raw'))
+        self.assertTrue(glance.glance_exists(_IMG_NAME))
+        self.assertTrue(glance.main(['-v', '-d', _IMG_NAME]))
+        self.assertFalse(glance.glance_exists(_IMG_NAME))
+
+    def test_main_fail_wrong_param_delete(self):
+        self.assertFalse(glance.glance_exists(_IMG_NAME))
+        self.assertTrue(glance.glance_import('/dev/null', name=_IMG_NAME, diskformat='raw'))
+        self.assertTrue(glance.glance_exists(_IMG_NAME))
+        with self.assertRaises(SystemExit):
+            with utils.devnull('stderr'):
+                glance.main(['-d'])
+        self.assertTrue(glance.glance_exists(_IMG_NAME))
+
+    def test_main_fail_wrong_param_name(self):
+        self.assertFalse(glance.glance_exists(_IMG_NAME))
+        self.assertTrue(glance.glance_import('/dev/null', name=_IMG_NAME, diskformat='raw'))
+        self.assertTrue(glance.glance_exists(_IMG_NAME))
+        with self.assertRaises(SystemExit):
+            with utils.devnull('stderr'):
+                glance.main([_IMG_NAME])
+        self.assertTrue(glance.glance_exists(_IMG_NAME))
+
+class TestGlanceMisc(TestGlanceFixture):
+
+    def test_exists_import(self):
+        self.assertFalse(glance.glance_exists(_IMG_NAME))
+        self.assertTrue(glance.glance_import('/dev/null', name=_IMG_NAME, diskformat='raw'))
+        self.assertTrue(glance.glance_exists(_IMG_NAME))
+
+    def test_delete_all(self):
+        self.assertFalse(glance.glance_exists(_IMG_NAME))
+        self.assertTrue(glance.glance_import('/dev/null', name=_IMG_NAME, diskformat='raw'))
+        self.assertTrue(glance.glance_import('/dev/null', name=_IMG_NAME, diskformat='raw'))
+        self.assertTrue(glance.glance_import('/dev/null', name=_IMG_NAME, diskformat='raw'))
+        self.assertTrue(glance.glance_exists(_IMG_NAME))
+        self.assertTrue(glance.glance_delete_all(_IMG_NAME))
+        self.assertFalse(glance.glance_exists(_IMG_NAME))
 
     def test_delete(self):
-        self.assertFalse(glance.glance_exists('test-glance-img'))
-        glance.glance_import('/dev/null', name='test-glance-img')
-        self.assertTrue(glance.glance_exists('test-glance-img'))
-        glance.glance_delete('test-glance-img')
-        self.assertFalse(glance.glance_exists('test-glance-img'))
+        self.assertFalse(glance.glance_exists(_IMG_NAME))
+        self.assertTrue(glance.glance_import('/dev/null', name=_IMG_NAME, diskformat='raw'))
+        self.assertTrue(glance.glance_exists(_IMG_NAME))
+        self.assertTrue(glance.glance_delete(_IMG_NAME))
+        self.assertFalse(glance.glance_exists(_IMG_NAME))
 
     def test_download(self):
         _RND1M_FILE = get_local_path('..', 'data', 'random_1M.bin')
-        self.assertFalse(glance.glance_exists('test-glance-img'))
-        glance.glance_import(_RND1M_FILE, name='test-glance-img', diskformat='raw')
-        self.assertTrue(glance.glance_exists('test-glance-img'))
-        self.assertTrue(glance.glance_download('test-glance-img', '/tmp/test-glance-img'))
-        self.assertTrue(utils.run(['cmp', _RND1M_FILE, '/tmp/test-glance-img'])[0])
-        os.remove('/tmp/test-glance-img')
-        self.assertFalse(glance.glance_download('', '/tmp/test-glance-img'))
+        _DL_ED_FILE = os.path.join('/', 'tmp', _IMG_NAME)
+
+        self.assertFalse(glance.glance_exists(_IMG_NAME))
+        self.assertTrue(glance.glance_import(_RND1M_FILE, name=_IMG_NAME, diskformat='raw'))
+        self.assertTrue(glance.glance_exists(_IMG_NAME))
+        self.assertTrue(glance.glance_download(_IMG_NAME, _DL_ED_FILE))
+        self.assertTrue(utils.run(['cmp', _RND1M_FILE, _DL_ED_FILE])[0])
+        os.remove(_DL_ED_FILE)
+        self.assertFalse(glance.glance_download('', _DL_ED_FILE))
+        self.assertFalse(os.path.exists(_DL_ED_FILE))
