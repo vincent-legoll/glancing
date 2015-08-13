d7c97c50885baa4ccd4ce48c4e4266d2ca3f0b80
Vincent Legoll <vincent.legoll@idgrilles.fr>
2015-08-13 16:21:00 +0200

    Robustify a test

--- test/src/test_glance.py
+++ test/src/test_glance.py
@@ -122,10 +122,13 @@ class TestGlanceMisc(TestGlanceFixture):
         _RND1M_FILE = get_local_path('..', 'data', 'random_1M.bin')
         _DL_ED_FILE = os.path.join('/', 'tmp', _IMG_NAME)
 
+        if os.path.exists(_DL_ED_FILE):
+            os.remove(_DL_ED_FILE)
         self.assertFalse(glance.glance_exists(_IMG_NAME))
         self.assertTrue(glance.glance_import(_RND1M_FILE, name=_IMG_NAME, diskformat='raw'))
         self.assertTrue(glance.glance_exists(_IMG_NAME))
         self.assertTrue(glance.glance_download(_IMG_NAME, _DL_ED_FILE))
+        self.assertTrue(os.path.exists(_DL_ED_FILE))
         self.assertTrue(utils.run(['cmp', _RND1M_FILE, _DL_ED_FILE])[0])
         os.remove(_DL_ED_FILE)
         self.assertFalse(glance.glance_download('', _DL_ED_FILE))
