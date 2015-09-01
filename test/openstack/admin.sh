9046bc84b7f79123c5e96f785df59c45d5f0db5e
Vincent Legoll <vincent.legoll@idgrilles.fr>
2015-09-01 16:11:02 +0200

    Whitespace fix

--- test/openstack/admin.sh
+++ test/openstack/admin.sh
@@ -7,4 +7,3 @@ export OS_TENANT_NAME=admin
 export OS_USERNAME=admin
 export OS_PASSWORD=ADMIN_PASS
 export OS_AUTH_URL=http://ctrl:35357/v2.0
-
--- test/openstack/demo.sh
+++ test/openstack/demo.sh
@@ -4,4 +4,3 @@ export OS_USERNAME=demo
 export OS_TENANT_NAME=demo
 export OS_PASSWORD=DEMO_PASS
 export OS_AUTH_URL=http://ctrl:5000/v2.0
-
