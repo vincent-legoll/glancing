bedceae2ff29a57e9ea57cb01558bcc05579d058
Vincent Legoll <vincent.legoll@idgrilles.fr>
2015-09-14 16:46:57 +0200

    Add local_pythonpath() test

--- test/src/test_tutils.py
+++ test/src/test_tutils.py
@@ -1,5 +1,6 @@
 #! /usr/bin/env python
 
+import sys
 import unittest
 
 import tutils
@@ -12,3 +13,14 @@ class TutilsTest(unittest.TestCase):
     def test_tutils_get_local_path(self):
         lp = tutils.get_local_path('toto', 'titi.txt')
         self.assertTrue(lp.endswith('/glancing/test/src/toto/titi.txt'))
+
+    def test_tutils_local_pythonpath(self):
+        def count_endswith(alist, suffix):
+            return len([x for x in alist if x.endswith(suffix)])
+        self.assertEqual(0, count_endswith(sys.path, 'TOTO'))
+        tutils.local_pythonpath('TOTO')
+        self.assertEqual(1, count_endswith(sys.path, 'TOTO'))
+        tutils.local_pythonpath('TOTO')
+        self.assertEqual(1, count_endswith(sys.path, 'TOTO'))
+        sys.path.remove(tutils.get_local_path('TOTO'))
+        self.assertEqual(0, count_endswith(sys.path, 'TOTO'))
