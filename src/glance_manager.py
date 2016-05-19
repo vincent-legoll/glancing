e5c82640e32afd3dc95d27826a3247e220a2fa9b
Vincent Legoll <vincent.legoll@idgrilles.fr>
2016-05-19 09:35:18 +0200

    pylint fixes & cleanups

--- src/glance_manager.py
+++ src/glance_manager.py
@@ -1,11 +1,13 @@
 #! /usr/bin/env python
 
+''' Utility script to automatically distribute cloud images from the
+France Grilles marketplace.
+'''
+
 from __future__ import print_function
 
 import os
-import re
 import sys
-import tempfile
 import textwrap
 import argparse
 
@@ -13,7 +15,6 @@ import utils
 import glance
 import glancing
 import metadata
-import multihash
 import openstack_out
 
 from utils import vprint
@@ -21,8 +22,9 @@ from utils import vprint
 _DEFAULT_VMLIST_FILE = os.path.join('/', 'etc', 'glancing', 'vmlist')
 _DEFAULT_SL_MARKETPLACE_URL_BASE = 'https://marketplace.stratuslab.eu/marketplace/metadata/'
 
-# Handle CLI options
 def do_argparse(sys_argv):
+    '''Handle CLI options
+    '''
     desc_help = textwrap.dedent('''
         Manage the list of endorsed images to be distributed across all
         IdGC / France-Grilles sites.
@@ -44,6 +46,8 @@ def do_argparse(sys_argv):
     return args
 
 def get_vmlist(vmlist_fn):
+    '''Get list of StratusLab ID of images to download
+    '''
     ret = []
     with open(vmlist_fn, 'rb') as vmlist_f:
         for line in vmlist_f:
@@ -51,70 +55,56 @@ def get_vmlist(vmlist_fn):
     return ret
 
 def get_glance_images():
+    '''Get info about images already in glance
+    '''
     ret = {}
     ids = glance.glance_ids()
     for vmid in ids:
-        img = glance.glance_run('image-show', vmid)
+        img = glance.glance_show(vmid)
         if img:
             vmmap = openstack_out.map_block(img)
             ret[vmmap['checksum']] = vmmap
             ret[vmmap['name']] = vmmap
     return ret
 
-#~ $ glance image-show 0682c532-5c6b-4933-9657-9ea6bf93ce43
-#~ +------------------+--------------------------------------+
-#~ | Property         | Value                                |
-#~ +------------------+--------------------------------------+
-#~ | checksum         | 79b4436412283bb63c2cba4ac796bcd9     |
-#~ | container_format | bare                                 |
-#~ | created_at       | 2015-07-21T12:24:17                  |
-#~ | deleted          | False                                |
-#~ | disk_format      | raw                                  |
-#~ | id               | 0682c532-5c6b-4933-9657-9ea6bf93ce43 |
-#~ | is_public        | False                                |
-#~ | min_disk         | 0                                    |
-#~ | min_ram          | 0                                    |
-#~ | name             | cirros-0.3.4-i386                    |
-#~ | owner            | 0d932173c3504d0a93716329ceef753a     |
-#~ | protected        | False                                |
-#~ | size             | 12506112                             |
-#~ | status           | active                               |
-#~ | updated_at       | 2015-07-21T12:24:17                  |
-#~ +------------------+--------------------------------------+
-#~ --is-public [True|False]
-    #~ Makes an image accessible for all the tenants (admin-only by default).
-#~ --is-protected [True|False]
-    #~ Prevents an image from being deleted.
-
 def get_meta_file(vmid):
+    '''Retrieve image metadata from StratusLab marketplace, in XML format
+    '''
     # Get xml metadata file from StratusLab marketplace
     metadata_url_base = _DEFAULT_SL_MARKETPLACE_URL_BASE
-    fn = glancing.get_url(metadata_url_base + vmid)
-    os.rename(fn, fn + '.xml')
-    return fn + '.xml'
+    fn_meta = glancing.get_url(metadata_url_base + vmid)
+    os.rename(fn_meta, fn_meta + '.xml')
+    return fn_meta + '.xml'
 
 def handle_vm(vmid, vmmap):
+    '''Handle one image given by its SL marketplace ID
+    '''
+    vprint('handle_vm(%s)' % vmid)
     meta_file = get_meta_file(vmid)
     meta = metadata.MetaStratusLabXml(meta_file)
-    md = meta.get_metadata()
-    new_md5 = md['checksums']['md5']
-    vprint('handle_vm(%s)' % vmid)
+    mdata = meta.get_metadata()
+    new_md5 = mdata['checksums']['md5']
     if new_md5 in vmmap:
-        vprint('Already in local glance: %s' % vmmap[new_md5]['name'])
+        vprint('An image with the same MD5 is already in glance: ' + vmid)
+        #~ vmmap[new_md5]['name']
         return
 
     name = meta.get_name()
     if name in vmmap:
-        vprint('An image with the same name is already in glance, but md5 differ')
+        vprint('An image with the same name is already in glance, '
+               'but md5 differ: ' + vmid)
         if not glance.glance_rename(vmid, name + '_old'):
             return
 
     glancing.main(['-v', '-s', new_md5, '-n', name, meta_file])
 
 def main(sys_argv=sys.argv[1:]):
+    '''Download images specified in the given list & store them in glance
+    '''
     args = do_argparse(sys_argv)
-    vprint(args.vmlist)
+    vprint('Image list: ' + args.vmlist)
     if not args.vmlist or not os.path.exists(args.vmlist):
+        print('Cannot access image list file: ' + args.vmlist)
         return False
     vmmap = get_glance_images()
     vmlist = get_vmlist(args.vmlist)
