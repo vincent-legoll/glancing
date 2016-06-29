ac6fd1d2738f6d6cd47cccbac83e8398790b930c
Vincent Legoll <vincent.legoll@idgrilles.fr>
2016-06-29 16:12:53 +0200

    Fix line too long

--- test/src/test_glance_manager.py
+++ test/src/test_glance_manager.py
@@ -30,8 +30,11 @@ class GlanceManagerTest(unittest.TestCase):
         glance_manager.get_meta_file = _OLDGMF
 
     def test_glance_manager_mocked_ok(self):
-        with utils.cleanup(['glance', '--os-image-api-version', '1', 'image-delete', 'GLANCE_MANAGER_CIRROS_TESTING_IMAGE']):
-            self.assertTrue(glance_manager.main(['-v', '-l',
-                get_local_path('..', 'gm_list.txt')]))
+        cln = ['glance', '--os-image-api-version', '1', 'image-delete',
+               'GLANCE_MANAGER_CIRROS_TESTING_IMAGE']
+        with utils.cleanup(cln):
+            locpath = get_local_path('..', 'gm_list.txt')
+            ret = glance_manager.main(['-v', '-l', locpath])
+            self.assertTrue(ret)
 
 # TODO: test name with spaces characters (initial, update, etc...)
