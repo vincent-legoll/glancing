d94ccfe96f17fa3ffab773e484021db57b20284a
Vincent Legoll <vincent.legoll@idgrilles.fr>
2016-10-12 14:38:47 +0200

    Better module docstring

--- src/glance_manager.py
+++ src/glance_manager.py
@@ -16,7 +16,8 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-''' Utility script to automatically distribute cloud images from the
+'''
+Utility script to automatically distribute cloud images from the
 France Grilles marketplace.
 
 Manage the list of endorsed images to be distributed across all
