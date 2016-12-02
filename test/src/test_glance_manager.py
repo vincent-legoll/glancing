b78442a92a6bf01eb10e075bbd75a2a1a47df116
Vincent Legoll <vincent.legoll@idgrilles.fr>
2016-12-02 13:03:17 +0100

    Fix _GLANCE_OK for test skipping

--- test/src/test_glance_manager.py
+++ test/src/test_glance_manager.py
@@ -8,6 +8,7 @@ from tutils import local_pythonpath, get_local_path
 local_pythonpath('..', '..', 'src')
 
 import utils
+import glance
 import glance_manager
 
 utils.set_verbose(True)
@@ -15,7 +16,7 @@ utils.set_verbose(True)
 # Check we have a cloud ready to import images into...
 _GLANCE_OK = False
 with utils.devnull('stderr'):
-    _GLANCE_OK = utils.run(['glance', 'image-list'])[0]
+    _GLANCE_OK = glance.glance_run('image-list') is not None
 
 _OLDGMF = glance_manager.get_meta_file
 
